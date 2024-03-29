529412ab0ed70da895ae41530d754d92b8c23ac0
Jayvee Empleo <geo.jayveeempleo@gmail.com>
2024-03-30 00:50:20 +0800

    done Auto writer

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,4 +5,54 @@ import { Component } from '@angular/core';
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+    message: string = 'Im Jayvee Empleo';
+    message2: string = 'Im Armie Empleo';
+
+    constructor() {}
+
+    ngOnInit(): void {
+        this.typewriterEffect();
+    }
+
+    typewriterEffect(): void {
+        const element = document.getElementById('myElement');
+        if (element) {
+            const text = this.message;
+            let i = 0;
+            element.innerHTML = 'Hello '; // Clear existing content
+            const typing = setInterval(() => {
+                if (i < text.length) {
+                    element.innerHTML += text.charAt(i);
+                    i++;
+                } else {
+                    clearInterval(typing);
+                    setTimeout(() => {
+                        // Reset the typewriter effect after a delay (e.g., 3 seconds)
+                        this.typewriterEffect2();
+                    }, 100); // Adjust the delay as needed
+                }
+            }, 100); // Adjust the interval to change typing speed
+        }
+    }
+    typewriterEffect2(): void {
+        const element = document.getElementById('myElement');
+        if (element) {
+            const text = this.message2;
+            let i = 0;
+            element.innerHTML = 'Hello '; // Clear existing content
+            const typing = setInterval(() => {
+                if (i < text.length) {
+                    element.innerHTML += text.charAt(i);
+                    i++;
+                } else {
+                    clearInterval(typing);
+                    setTimeout(() => {
+                        // Reset the typewriter effect after a delay (e.g., 3 seconds)
+                        this.typewriterEffect();
+                    }, 100); // Adjust the delay as needed
+                }
+            }, 100); // Adjust the interval to change typing speed
+        }
+    }
+}
